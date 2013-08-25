c4543d05d3d489b7c5e7eb961984ebcb5513275c
Everton Ribeiro <nuxlli@gmail.com>
2013-08-25 12:57:19 -0300

    Adding test to Rethinkdb.connect support a parameters.

--- test/rethinkdb_test.exs
+++ test/rethinkdb_test.exs
@@ -2,8 +2,16 @@ defmodule RethinkdbTest do
   use ExUnit.Case
   alias Rethinkdb, as: R
 
-  test "return a connection object to r" do
+  test "return a connection record" do
     conn = R.connect
     assert is_record(conn, R.Connection)
+  end
+
+  test "return a connection with parameters" do
+    conn = R.connect(host: "example.com")
+    assert "example.com" == conn.host
+
+    conn = R.connect("rethinkdb://example.com")
+    assert "example.com" == conn.host
   end
 end
